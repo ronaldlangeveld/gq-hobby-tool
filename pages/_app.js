29efb11eb5c30dbdb27bbd64a94970a1ec059b4f
Ronald Langeveld <kook@Ronalds-MacBook-Pro.local>
2021-05-19 16:09:40 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,26 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    padding: 0;
+  html {
     box-sizing: border-box;
+
+  }
+  body{
+    position: relative;
+    font-size: 1.4rem;
+    /* line-height: 2.1rem; */
+    font-family: "jaf-domus";
+    margin:0;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
   }
 `
 
 const theme = {
   colors: {
-    primary: '#0070f3',
+    primary: '#E2574C',
+    dark: '#323032',
+    light: '#ebebe9'
   },
 }
 
