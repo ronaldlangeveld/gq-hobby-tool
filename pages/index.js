119608440551df3c0a7fb0b342343c2abb21847b
Ronald Langeveld <kook@Ronalds-MBP.lan>
2021-07-19 12:26:31 +0200

    convert to SSG

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-import styled from 'styled-components';
+// import styled from 'styled-components';
 import Header from '../components/section/header';
 import Results from '../components/section/results';
 import {getDatabase} from '../api/notion';
 import {useState, useEffect} from 'react';
-import {tags} from '../api/tags';
+// import {tags} from '../api/tags';
 import {useRouter} from 'next/router';
 
 const Index = ({data, taglist, queries}) => {
 
-  const router= useRouter();
+  const router = useRouter();
 
   const [selected, setSelected] = useState([]);
 
@@ -45,16 +45,25 @@ export default Index;
 
 
 
-export async function getServerSideProps(context){
+export async function getStaticProps(context){
   const data = await getDatabase(process.env.NOTION_DATABASE_ID);
-  const taglist = tags;
-  console.log(context.query);
-
-  const queries = context.query.tags || []
+  const taglist = [
+    "Being Active",
+    "Achievement",
+    "Relaxing",
+    "Being Social",
+    "Mentally Engaging",
+    "Creativity",
+    "Free",
+    "Low Cost",
+    "At Home",
+    "Offline",
+    "Cam's Favorites",
+    "Easy to Start"
+  ]
 
   return {props: {
     data,
     taglist,
-    queries
   }};
 }
